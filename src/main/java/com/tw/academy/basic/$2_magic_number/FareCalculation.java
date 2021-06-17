87df6edb4b1abc7d88c351414ba4ea65cbc2980c
Demi Cao <demi.cao@oocl.com>
2021-06-17 11:46:19 +0800

    [Demi] - refactor magic number

--- src/main/java/com/tw/academy/basic/$2_magic_number/FareCalculation.java
+++ src/main/java/com/tw/academy/basic/$2_magic_number/FareCalculation.java
@@ -2,12 +2,19 @@ package com.tw.academy.basic.$2_magic_number;
 
 public class FareCalculation {
 
+    private static final int cumulativeExpensesClass1Min = 100;
+    private static final int cumulativeExpensesClass1Max = 150;
+    private static final double cumulativeExpensesClass1Discount = 0.8;
+    private static final int cumulativeExpensesClass2Min = 150;
+    private static final int cumulativeExpensesClass2Max = 400;
+    private static final double cumulativeExpensesClass2Discount = 0.5;
+
     public double calculate(double originalPrice, double cumulativeExpensesThisMonth) {
-        if (cumulativeExpensesThisMonth >= 100 && cumulativeExpensesThisMonth < 150) {
-            return originalPrice * 0.8;
+        if (cumulativeExpensesThisMonth >= cumulativeExpensesClass1Min && cumulativeExpensesThisMonth < cumulativeExpensesClass1Max) {
+            return originalPrice * cumulativeExpensesClass1Discount;
         }
-        if (cumulativeExpensesThisMonth >= 150 && cumulativeExpensesThisMonth < 400) {
-            return originalPrice * 0.5;
+        if (cumulativeExpensesThisMonth >= cumulativeExpensesClass2Min && cumulativeExpensesThisMonth < cumulativeExpensesClass2Max) {
+            return originalPrice * cumulativeExpensesClass2Discount;
         }
         return originalPrice;
     }
