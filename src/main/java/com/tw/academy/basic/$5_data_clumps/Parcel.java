065e038875ce0a365ad89dac3ae66c3c3eaa67a5
Demi Cao <demi.cao@oocl.com>
2021-06-17 16:25:10 +0800

    [Demi] - refactor inline receiver and sender

--- src/main/java/com/tw/academy/basic/$5_data_clumps/Parcel.java
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/Parcel.java
@@ -4,35 +4,27 @@ public class Parcel {
     private String size;
     private Double weight;
 
-    private String receiverName;
-    private String receiverPhoneNumber;
-    private String receiverAddress;
+    private final User receiver;
+    private final User sender;
 
-    private String senderName;
-    private String senderPhoneNumber;
-    private String senderAddress;
 
     public Parcel(String size, Double weight,
-                  User user, User user1) {
+                  User receiver, User sender) {
         this.size = size;
         this.weight = weight;
-        this.receiverName = user.getName();
-        this.receiverPhoneNumber = user.getPhoneNumber();
-        this.receiverAddress = user.getReceiverAddress();
-        this.senderName = user1.getName();
-        this.senderPhoneNumber = user1.getPhoneNumber();
-        this.senderAddress = user1.getReceiverAddress();
+        this.receiver = receiver;
+        this.sender = sender;
     }
 
     public String confirmReceiver(){
         return String.format("Please confirm receiver information: receiver name is %s, " +
                         "receiver phone number is %s and receiver address is %s.\n",
-                receiverName, receiverPhoneNumber, receiverAddress);
+                receiver.getName(), receiver.getPhoneNumber(), receiver.getReceiverAddress());
     }
 
     public String confirmSender(){
         return String.format("Please confirm sender information: sender name is %s, " +
                         "sender phone number is %s and sender address is %s.\n",
-                senderName, senderPhoneNumber, senderAddress);
+                sender.getName(), sender.getPhoneNumber(), sender.getReceiverAddress());
     }
 }
